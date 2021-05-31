7e17fe32473127cc9331a445c339e65946059b27
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-05-31 16:27:43 +0800

    refactor: add environmental variables for api keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyBJXl4OgEzZsjVYruQrHm0pWk0qSjYNYUA",
-  authDomain: "plan-et-6be4e.firebaseapp.com",
-  projectId: "plan-et-6be4e",
-  storageBucket: "plan-et-6be4e.appspot.com",
-  messagingSenderId: "238966307669",
-  appId: "1:238966307669:web:a3ea99527b942386b0cddc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
-
 export const auth = firebaseConfig.auth();
 export default firebaseConfig;
