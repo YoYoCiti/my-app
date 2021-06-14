10bd74c5d08f4f64fa2323e2492f67656b1a6377
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-06-14 15:52:10 +0800

    refactor: refactor Planner code
    Remove PlannerList from PlannerManager and create separate component folder for PlannerList

--- src/components/PlannerManager/PlannerManager.js
+++ src/components/PlannerManager/PlannerManager.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./PlannerManager.css";
-import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
 import ModuleBar from "../ModuleBar";
-import { BsX } from "react-icons/bs";
 import { database } from "../../config/firebase";
 import { useAuth } from "../../contexts/AuthContext";
+import PlannerList from "../PlannerList";
 
 function PlannerManager(props) {
   const { moduleData } = props;
@@ -54,71 +52,6 @@ function PlannerManager(props) {
         semSelected={semSelected}
       />
     </div>
-  );
-}
-
-function PlannerList(props) {
-  const { setModuleBar, plannedModules, setPlannedModules, setSemSelected } =
-    props;
-  const { currentUser } = useAuth();
-
-  function handleRemoveModule(sem, mod) {
-    const newPlannedModules = [
-      ...plannedModules.slice(0, sem),
-      {
-        acadSemester: [
-          ...plannedModules[sem].acadSemester.slice(0, mod),
-          ...plannedModules[sem].acadSemester.slice(mod + 1),
-        ],
-      },
-      ...plannedModules.slice(sem + 1),
-    ];
-
-    setPlannedModules(newPlannedModules);
-    database.users
-      .doc(currentUser?.uid)
-      .update({ plannedModules: newPlannedModules });
-  }
-
-  return (
-    <>
-      {!plannedModules ? (
-        <div className="temp-text">Loading...</div>
-      ) : (
-        plannedModules.map((sem, index1) => (
-          <>
-            <h2>
-              Year {Math.floor(index1 / 2) + 1} Sem {index1 % 2 === 0 ? 1 : 2}
-            </h2>
-            <CardDeck key={index1}>
-              {sem.acadSemester.map((module, index2) => (
-                <Card
-                  key={index2}
-                  style={{ width: "10rem" }}
-                  onClick={
-                    module.title === "Add Modules"
-                      ? () => {
-                          setModuleBar(true);
-                          setSemSelected(index1);
-                        }
-                      : () => false
-                  }
-                >
-                  {module.title !== "Add Modules" && (
-                    <BsX
-                      className="remove-button"
-                      onClick={() => handleRemoveModule(index1, index2)}
-                    />
-                  )}
-                  <Card.Title>{module.moduleCode}</Card.Title>
-                  <Card.Text>{module.title}</Card.Text>
-                </Card>
-              ))}
-            </CardDeck>
-          </>
-        ))
-      )}
-    </>
   );
 }
 
