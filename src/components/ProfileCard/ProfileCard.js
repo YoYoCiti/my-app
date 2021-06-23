54ca84743c537239eb1a9ff7d87dd3d0ac48784b
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-06-23 15:36:26 +0800

    Update profile card to display username

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
+import { database } from "../../config/firebase";
 import "./ProfileCard.css";
 
 function ProfileCard() {
   const { currentUser } = useAuth();
+  const [username, setUsername] = useState();
+  useEffect(() => {
+    database.users
+      .doc(currentUser?.uid)
+      .get()
+      .then((doc) => {
+        setUsername(doc.data().username);
+      });
+  }, [currentUser]);
+
   return (
     <div className="main-container">
       <div className="profile-card">
@@ -11,6 +23,7 @@ function ProfileCard() {
             Profile Picture currently not supported
           </div>
           <h1 style={{ padding: "1rem 0 1rem 0" }}>My Profile</h1>
+          <p>Username: {username}</p>
           <p>Email: {currentUser.email}</p>
         </div>
       </div>
