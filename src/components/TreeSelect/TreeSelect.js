82e570aa51883a2e7e81fc791a0a4fc2e27c2385
Xiu Wen <xiuwenseow@yahoo.com.sg>
2021-07-13 17:46:52 +0800

    feat: Add Module and Exempt button in treeselect component
    Functionality not done.

--- src/components/TreeSelect/TreeSelect.js
+++ src/components/TreeSelect/TreeSelect.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { TreeView } from "@progress/kendo-react-treeview";
+import { DropDownButton } from "@progress/kendo-react-buttons";
+import { Button } from "react-bootstrap";
 import "@progress/kendo-theme-default/dist/all.css";
 import "./TreeSelect.css";
 
@@ -11,7 +13,14 @@ function TreeSelect(props) {
     }
     const newTree = tree.map((obj) => {
       return {
-        text: typeof obj === "string" ? obj : "or" in obj ? "OR" : "AND",
+        text:
+          typeof obj === "string" ? (
+            <TreeItem item={obj} />
+          ) : "or" in obj ? (
+            "OR"
+          ) : (
+            "AND"
+          ),
         expanded: true,
         items:
           typeof obj === "string"
@@ -24,10 +33,6 @@ function TreeSelect(props) {
     return newTree;
   };
 
-  const onItemClick = (event) => {
-    event.item.selected = !event.item.selected;
-  };
-
   const onExpandChange = (event) => {
     event.item.expanded = !event.item.expanded;
   };
@@ -36,9 +41,43 @@ function TreeSelect(props) {
       data={transform(alertState.tree)}
       expandIcons={true}
       onExpandChange={onExpandChange}
-      onItemClick={onItemClick}
       className="tree"
     />
   );
 }
+
+function TreeItem(props) {
+  const { item } = props;
+  const dropdownItems = [
+    "Y1S1",
+    "Y1S2",
+    "Y2S1",
+    "Y2S2",
+    "Y3S1",
+    "Y3S2",
+    "Y4S1",
+    "Y4S2",
+  ];
+
+  function handleAddModule(sem) {
+    console.log(sem);
+  }
+
+  return (
+    <div className="item-container">
+      <span className="item">{item}</span>
+      <DropDownButton
+        text="Add Module"
+        className="tree-item-button"
+        primary={true}
+        items={dropdownItems}
+        onItemClick={(event) => handleAddModule(event.itemIndex)}
+      />
+      <Button variant="secondary" className="tree-item-button" size="sm">
+        Exempt/Took Equivalent
+      </Button>
+    </div>
+  );
+}
+
 export default TreeSelect;
