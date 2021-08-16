daebc648f8adced07b9d1b70dab4a1ba0cba1461
Yirui <72332263+Zhouyirui@users.noreply.github.com>
2021-08-16 21:16:47 +0800

    Able to delete comments

--- src/components/Board/Comment.js
+++ src/components/Board/Comment.js
@@ -1,29 +1,62 @@
-import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
 import TimeAgo from "react-timeago";
 import styles from "./Board.module.css";
+import { BiTrashAlt } from "react-icons/bi";
+import Modal from "react-bootstrap/Modal";
+import { Button } from "react-bootstrap";
+import { database } from "../../config/firebase";
 
 function Comment(props) {
-  const { thread, formatter } = props;
-  const history = useHistory();
+  const { thread, formatter, newPostUser } = props;
+  const [show, setShow] = useState(false);
 
-  function handleClick() {
-    history.push(`/board/${thread.id}`);
+  function deleteComment() {
+    database.board
+      .doc(thread.threadId)
+      .collection("post")
+      .doc(thread.postId)
+      .delete()
+      .then(() => console.log("successfully deleted comment"));
   }
-
   return (
-    <div className={styles.threadBox} key={thread.id} onClick={handleClick}>
-      <div className={styles.threadUser}>
-        {thread.user}&nbsp;
-        <TimeAgo
-          date={thread.timeDisplay}
-          formatter={formatter}
-          minPeriod="MINUTE"
-          className={styles.threadTime}
-        />{" "}
+    <div className={styles.threadBox} key={thread.id}>
+      <div className={styles.threadBoxL}>
+        <div className={styles.threadUser}>
+          {thread.user}&nbsp;
+          <TimeAgo
+            date={thread.timeDisplay}
+            formatter={formatter}
+            minPeriod="MINUTE"
+            className={styles.threadTime}
+          />{" "}
+        </div>
+        {/* <div className={styles.threadTitle}>{thread.title}</div> */}
+        <div className={styles.threadText}>{thread.content}</div>
       </div>
-      <div className={styles.threadTitle}>{thread.title}</div>
-      <div className={styles.threadText}>{thread.content}</div>
+      {newPostUser === thread.user && (
+        <div className={styles.threadBoxR}>
+          <BiTrashAlt
+            size={22}
+            className={styles.threadIcon}
+            onClick={() => setShow(true)}
+          />{" "}
+        </div>
+      )}
       {/* <div className={styles.threadTime}>{thread.timeDisplay}</div> */}
+      <Modal centered show={show} onHide={() => setShow(false)}>
+        <Modal.Body>
+          Are you sure you want to delete this thread? This action cannot be
+          undone.{" "}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={deleteComment}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
